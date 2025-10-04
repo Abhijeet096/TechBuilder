75513707d7cbf00f49b20d77bdc47cf318a2f63e
Abhijeet096 <153988621+Abhijeet096@users.noreply.github.com>
2025-10-04 11:00:16 +0530

    Delete app directory

--- app/src/components/Loader.jsx
+++
@@ -1,7 +0,0 @@
-export default function Loader({ className = '' }) {
-  return (
-    <div className={`grid place-items-center ${className}`}>
-      <div className="h-10 w-10 animate-spin rounded-full border-2 border-primary border-t-transparent" />
-    </div>
-  )
-}
